4a6f8eed6b49c0dec4995f360649984e62859962
Krishna Kant Dadhich <krishnakantdadhich47@gmail.com>
2021-07-18 09:25:28 +0530

    Validators updated

--- src/app/features/user/user.component.ts
+++ src/app/features/user/user.component.ts
@@ -29,13 +29,6 @@ export class UserComponent implements OnInit {
   ngOnInit(): void {
     this.subscriberAdd()
     this.addsubscriberForm()
-    // this.productService.getProducts().then(data => this.products = data);
-
-    //   this.statuses = [
-    //     {label: 'INSTOCK', value: 'instock'},
-    //     {label: 'LOWSTOCK', value: 'lowstock'},
-    //     {label: 'OUTOFSTOCK', value: 'outofstock'}
-    // ];
   }
 
 
@@ -45,7 +38,7 @@ export class UserComponent implements OnInit {
       name: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
       fatherName: ['', [Validators.required]],
-      address: ['', [Validators.required, Validators.minLength(10)]],
+      address: ['', [Validators.required]],
       city: ['', [Validators.required]],
       mobile: ['', [Validators.required, Validators.minLength(10)]],
       pinCode: ['', [Validators.required]],
@@ -124,46 +117,13 @@ export class UserComponent implements OnInit {
     this.subscriberDialog = true;
     this.subscriberForm.addControl['_id'];
     this.subscriberForm.patchValue(data);
-    // const reqData = {
-    //   "name" : this.subscriberForm.value.name,
-    //   "email" : this.subscriberForm.value.email,
-    //   "fatherName":this.subscriberForm.value.fatherName,
-    //   "address" : this.subscriberForm.value.address,
-    //   "city" : this.subscriberForm.value.city,
-    //   "mobile" : this.subscriberForm.value.mobile,
-    //   "pinCode" : this.subscriberForm.value.pinCode,
-    //   "state" : this.subscriberForm.value.state,
-    //   "_id":"60ea65423dcb02066fcdcc36"
-    // }
-
-    // this.subscriberService.subscriberUpdate(reqData).subscribe(arg => {
-    //   this.subscriberAdd();
-    // })
-
   }
 
   openNew() {
-   
     this.submitted = false;
     this.subscriberDialog = true;
   }
 
-
-
-
-  deleteProduct(product: any) {
-    this.confirmationService.confirm({
-      message: 'Are you sure you want to delete ' + product.name + '?',
-      header: 'Confirm',
-      icon: 'pi pi-exclamation-triangle',
-
-      // accept: () => {
-      //     this.products = this.products.filter(val => val.id !== product.id);
-      //     this.product = {};
-      //     this.messageService.add({severity:'success', summary: 'Successful', detail: 'Product Deleted', life: 3000});
-      // }
-    });
-  }
 
   hideDialog() {
     this.subscriberDialog = false;
@@ -171,27 +131,6 @@ export class UserComponent implements OnInit {
   }
 
 
-
-  //   findIndexById(id: string): number {
-  //     let index = -1;
-  //     for (let i = 0; i < this.products.length; i++) {
-  //         if (this.products[i].id === id) {
-  //             index = i;
-  //             break;
-  //         }
-  //     }
-
-  //     return index;
-  // }
-
-  // createId(): string {
-  //     let id = '';
-  //     var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  //     for ( var i = 0; i < 5; i++ ) {
-  //         id += chars.charAt(Math.floor(Math.random() * chars.length));
-  //     }
-  //     return id;
-  // }
   updatesubscriberData() {
     this.subscriberService.subscriberUpdate(this.subscriberForm.value).subscribe(arg => {
       this.subscriberDialog = false;
